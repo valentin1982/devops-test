b825406014db2490e7443a1aa7a4751a3bbf04bc
vzaykov <valentyn.zaikov@quartsoft.com>
2025-08-07 22:15:38 +0200

    add MetricsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisService } from './redis/redis.service';
+import { MetricsModule } from './metrics/metrics.module'; // Ensure this path is correct based on your project structure
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
 		}),
+		MetricsModule, // Import the MetricsModule here
 	],
 	controllers: [AppController],
 	providers: [AppService, RedisService],
 })
-export class AppModule { }
+export class AppModule {}
